81974616989a278d161fd339de4d7b3062e54f97
Asharppen <asharppen@gmail.com>
2023-08-24 13:20:00 +0200

    Init project

---
+++ src/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+
+namespace LessZdoCorruption;
+
+[BepInPlugin(ModId, PluginName, Version)]
+public class Plugin : BaseUnityPlugin
+{
+    public const string ModId = "LessZdoCorruption";
+    public const string PluginName = "Less ZDO Corruption";
+    public const string Version = "0.0.1";
+
+    private void Awake()
+    {
+
+    }
+}
